8d0068ad7ba65f2026415b408a974f2122ad9b5b
刘鑫 <admin@54xavier.cn>
2023-11-09 14:52:12 +0800

    fix check logs/*.log before write log

--- src/log.js
+++ src/log.js
@@ -1,25 +1,89 @@
 /*
  * @Date: 2023-09-29 20:50:59
  * @LastEditors: admin@54xavier.cn
- * @LastEditTime: 2023-10-02 11:29:49
- * @FilePath: /node-hiprint-transit/log.js
+ * @LastEditTime: 2023-11-09 14:35:40
+ * @FilePath: \node-hiprint-transit\src\log.js
  */
-import { appendFile } from "node:fs";
+import { access, appendFile, constants, mkdir, writeFile } from "node:fs";
 import dayjs from "dayjs";
 
-
-function log(message) {
+/**
+ * @description: This function checks if the log directory exists. If it does not exist, a new directory will be created.
+ * @return {Promise} A Promise object that resolves if the directory exists, or rejects if creating the directory fails.
+ */
+function checkDir() {
+  const dirPath = "./logs";
   return new Promise((resolve, reject) => {
-    const logMessage = `${dayjs().format("YYYY/MM/DD HH:mm:ss")}: ${message}\n`;
-    appendFile(
-      `logs/${dayjs().format("YYYY-MM-DD")}.log`,
-      logMessage,
-      (err) => {
-        if (err) reject(err);
+    access(dirPath, constants.F_OK, (err) => {
+      if (err) {
+        mkdir(dirPath, (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      } else {
         resolve();
       }
-    );
-  })
+    });
+  });
+}
+
+/**
+ * This function checks if a log file exists. If it does not exist, a new log file will be created.
+ * @returns {Promise} A Promise object that resolves if the file exists, or rejects if creating the file fails.
+ */
+function checkLogFile() {
+  const filePath = `./logs/${dayjs().format("YYYY-MM-DD")}.log`;
+  return new Promise((resolve, reject) => {
+    checkDir()
+      .then(() => {
+        access(filePath, constants.F_OK, (err) => {
+          if (err) {
+            writeFile(filePath, "", (err) => {
+              if (err) {
+                reject(err);
+              } else {
+                resolve;
+              }
+            });
+          } else {
+            resolve();
+          }
+        });
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
+
+/**
+ * Writes log message to log file.
+ * @param {string} message - The log message to be written.
+ * @returns {Promise} - A Promise object that resolves when writing is successful, or rejects when writing fails.
+ */
+function log(message) {
+  const filePath = `./logs/${dayjs().format("YYYY-MM-DD")}.log`;
+  return new Promise((resolve, reject) => {
+    checkLogFile()
+      .then(() => {
+        const logMessage = `${dayjs().format(
+          "YYYY/MM/DD HH:mm:ss"
+        )}: ${message}\n`;
+        appendFile(filePath, logMessage, (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
 }
 
 export default log;
